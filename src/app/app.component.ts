459d280dab63af9a5ed69a348e692a38b2cd47f9
Wisthong Martinez <106920956+Wisthong@users.noreply.github.com>
2023-12-28 16:21:14 -0500

    Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, DecimalPipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, DecimalPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'weather-app';
+  ano!: number;
+  ngOnInit(): void {
+    const anio = new Date();
+    this.ano = anio.getFullYear();
+  }
 }
