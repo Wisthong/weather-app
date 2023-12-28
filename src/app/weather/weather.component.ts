b91910d9111e7716ec5944fb2883ff50f95383ba
Wisthong Martinez <106920956+Wisthong@users.noreply.github.com>
2023-12-28 10:13:58 -0500

    PWA wheater

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
   imports: [],
   templateUrl: './weather.component.html',
-  styleUrl: './weather.component.css'
+  styleUrl: './weather.component.css',
 })
 export class WeatherComponent {
-
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
